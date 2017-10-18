ff958bba16ae77fa2fa28dfe226b5f42164df339
efosa <efosaokpugie@gmail.com>
2017-10-18 16:45:50 +0100

    app server house

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const mongoClientConnect = require('mongo-client-connect');
+mongoClientConnect('mongodb://dgcbooks-api:DGC12345@ds121225.mlab.com:21225/dgcbooks-api').then(db => {});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.set('port' , process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+	console.log('app running on port '+""+app.get('port'));
+});
+
+module.exports = app;
